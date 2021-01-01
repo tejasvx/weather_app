24720837769abc514d1047c38bc0253ca3ed5e1e
tejasv agarwal <tejasv.agarwal@girnarsoft.com>
2021-01-02 00:37:34 +0530

    added API functionality

--- src/app.js
+++ src/app.js
@@ -1,52 +1,69 @@
-const path = require('path')
-const express = require('express');
-const hbs = require('hbs')
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const getTemprature = require("./temprature.js");
 
-const app = express()
+const app = express();
 
 //Define paths
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) =>{
-  res.render('index', {
-    title: 'Tejasv App',
-    name: 'Tejasv Agarwal'    
-  })
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Tejasv App",
+    name: "Tejasv Agarwal",
+  });
+});
 
-app.get('/about', (req, res) =>{
-  res.render('about', {
-    title: 'About Me',
-    name: 'Tejasv Agarwal'    
-  })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Tejasv Agarwal",
+  });
+});
 
-app.get('/help', (req, res) =>{
-  res.render('help', {
-    helpText: 'This is some helpful text',
-    title: 'Help',
-    name: 'Tejasv Agarwal'    
-  })
-})
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text",
+    title: "Help",
+    name: "Tejasv Agarwal",
+  });
+});
 
-app.get('/weather', (req, res) =>{
-  res.send('weather')
-})
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "Please specify the location",
+    });
+  }
+  getTemprature(req.query.address, (error, temp) => {
+    if (error) {
+      return res.send({
+        error: error,
+      });
+    } else {
+      res.send({
+        temprature: temp,
+        location: req.query.address,
+      });
+    }
+  });
+});
 
-app.get('/*', (req, res) =>{
-  res.send('hehe')
-})
+app.get("/*", (req, res) => {
+  res.send("hehe");
+});
 
-app.listen(3000, () =>{
-  console.log('app is running');
-})
+app.listen(3000, () => {
+  console.log("app is running");
+});
---
+++ src/temprature.js
@@ -0,0 +1,16 @@
+const request = require("request");
+
+const getTemprature = (address, callback) => {
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${address}&APPID=37fb118f6c530e1209a19df9d3416f&units=metric`;
+  request({ url: url, json: true }, (error, res) => {
+    if (error) {
+      return callback("error while getting weather", null)
+    } else if (res.body.cod == "404") {
+      return callback(res.body.message, null)
+    } else {
+      return callback(null, res.body.main.temp);
+    }
+  });
+};
+
+module.exports =  getTemprature;
